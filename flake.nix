18a13e57b30298075c4d4b10629c376879aaa116
Anselm Schüler <mail@anselmschueler.com>
2022-01-20 19:07:56 +0100

    Add nix-lib to inputs in outputs

--- flake.nix
+++ flake.nix
@@ -1,21 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-lib.url = "github:schuelermine/nix-lib/b0";
     nix-symlink.url = "github:schuelermine/nix-symlink/b0";
   };
-  outputs = { self, nixpkgs, nix-symlink }: {
+  outputs = { self, nixpkgs, nix-symlink, nix-lib }: {
     nixosConfigurations.buggeryyacht-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs.symlink = nix-symlink.symlink;
-      modules = [
-        ./modules/boot.nix
-        ./modules/console.nix
-        ./modules/desktop.nix
-        ./modules/hardware.nix
-        ./modules/nvidia.nix
-        ./modules/system.nix
-        ./modules/users.nix
-      ];
+      modules = nix-lib.mapToValuesX
+        (name: type: if type != "regular" then [ ] else [ (./. + name) ])
+        (builtins.readDir ./modules);
     };
   };
 }
