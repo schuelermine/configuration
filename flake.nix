2ee4da816bc903b99ac3cff55bc17ceccdd65586
Anselm Schüler <mail@anselmschueler.com>
2022-01-20 19:43:41 +0100

    reform modules gathering

--- flake.nix
+++ flake.nix
@@ -4,13 +4,13 @@
     nix-lib.url = "github:schuelermine/nix-lib/b0";
     nix-symlink.url = "github:schuelermine/nix-symlink/b0";
   };
-  outputs = { self, nixpkgs, nix-symlink, nix-lib }: {
-    nixosConfigurations.buggeryyacht-nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs.symlink = nix-symlink.symlink;
-      modules = nix-lib.attrs.mapToValuesX
-        (name: type: if type != "regular" then [ ] else [ (./modules + "/${name}") ])
-        (builtins.readDir ./modules);
+  outputs = { self, nixpkgs, nix-symlink, nix-lib }:
+    with nix-lib; {
+      nixosConfigurations.buggeryyacht-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs.symlink = nix-symlink.symlink;
+        modules = attrs.mapToValues (n: _: ./modules + "/${n}") attrs.filter
+          (n: t: t == "regular") (builtins.readDir ./modules);
+      };
     };
-  };
 }
