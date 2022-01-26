418cbd959d7a6d4fffdd672e7113458d459001fd
Anselm Schüler <mail@anselmschueler.com>
2022-01-26 12:21:18 +0100

    Fix outputs args

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nix-lib.url = "github:schuelermine/nix-lib/b0";
     nix-symlink.url = "github:schuelermine/nix-symlink/b0";
   };
-  outputs = { self, nixpkgs, nix-symlink, nix-lib }:
+  outputs = { self, nixpkgs, nix-symlink, nixpkgs-fix-noto-cjk-src, nix-lib }:
     with nix-lib; {
       nixosConfigurations.buggeryyacht-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
