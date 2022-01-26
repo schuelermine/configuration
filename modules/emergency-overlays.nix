4eebd92960eec92b7d4d657eabec8a9c739fd961
Anselm Schüler <mail@anselmschueler.com>
2022-01-26 12:14:28 +0100

    try to fix emergency overlay

--- modules/emergency-overlays.nix
+++ modules/emergency-overlays.nix
@@ -5,9 +5,9 @@
       let
         mkNotoCJK = { typeface, version, rev, sha256 }:
           super.stdenvNoCC.mkDerivation {
-            pname = "noto-fonts-cjk-${lib.toLower typeface}";
+            pname = "noto-fonts-cjk-${super.lib.toLower typeface}";
             inherit version;
-            src = fetchFromGitHub {
+            src = super.fetchFromGitHub {
               owner = "googlefonts";
               repo = "noto-cjk";
               sparseCheckout = "${typeface}/OTC";
@@ -16,12 +16,12 @@
             installPhase = ''
               install -m444 -Dt $out/share/fonts/opentype/noto-cjk ${typeface}/OTC/*.ttc
             '';
-            meta = with lib; {
+            meta = {
               description = "Beautiful and free fonts for CJK languages";
               homepage = "https://www.google.com/get/noto/help/cjk/";
               longDescription = ''
                 Noto ${typeface} CJK is a ${
-                  lib.toLower typeface
+                  super.lib.toLower typeface
                 } typeface designed as
                 an intermediate style between the modern and traditional. It is
                 intended to be a multi-purpose digital font for user interface
@@ -33,9 +33,9 @@
                 Japanese kana, vertical forms, and variant characters (itaiji); it
                 supports Korean hangeul — both contemporary and archaic.
               '';
-              license = licenses.ofl;
-              platforms = platforms.all;
-              maintainers = with maintainers; [ mathnerd314 emily ];
+              license = super.noto-fonts-cjk-sans.meta.license;
+              platforms = super.noto-fonts-cjk-sans.meta.platforms;
+              maintainers = super.noto-fonts-cjk-sans.meta.maintainers;
             };
           };
       in {
