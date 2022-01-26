008f7e317e719b41e88ca3feb2f97eccff0f3665
Anselm Schüler <mail@anselmschueler.com>
2022-01-26 16:42:26 +0100

    fix & reword

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-fix-noto-cjk-src.url = "github:midchildan/nixpkgs/fix/noto-cjk-src";
+    nixpkgs-fix.url = "github:midchildan/nixpkgs/fix/noto-cjk-src";
     nix-lib.url = "github:schuelermine/nix-lib/b0";
     nix-symlink.url = "github:schuelermine/nix-symlink/b0";
   };
-  outputs = { self, nixpkgs, nix-symlink, nixpkgs-fix-noto-cjk-src, nix-lib }:
+  outputs = { self, nixpkgs, nix-symlink, nixpkgs-fix, nix-lib }:
     with nix-lib; {
       nixosConfigurations.buggeryyacht-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = {
-          nixpkgs-fix-noto-cjk-src = import nixpkgs-fix-noto-cjk-src { };
+          nixpkgs-fix = nixpkgs-fix.legacyPackages.x86_64-linux;
           symlink = nix-symlink.symlink;
         };
         modules = attrs.mapToValues (n: _: ./modules + "/${n}")
--- modules/emergency-overlays.nix
+++ modules/emergency-overlays.nix
@@ -1,9 +1,9 @@
-{ pkgs, nixpkgs-fix-noto-cjk-src }: {
+{ pkgs, nixpkgs-fix, ... }: {
   nixpkgs.overlays = [
     # This is to preemptively apply nixpkgs PR #156342 to fix Noto CJK downloads
     (self: super: {
-      inherit (nixpkgs-fix-noto-cjk-src)
-        noto-fonts-cjk-sans noto-fonts-cjk-serif;
+      noto-fonts-cjk-sans = nixpkgs-fix.noto-fonts-cjk-sans;
+      noto-fonts-cjk-serif = nixpkgs-fix.noto-fonts-cjk-serif;
     })
   ];
 }
