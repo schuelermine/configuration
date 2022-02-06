5e0496642eb7aca98f3f447347af189e7c4a481e
Anselm Schüler <mail@anselmschueler.com>
2022-02-06 10:28:19 +0100

    Remove emergency overlay
    This is a combination of 2 commits.
    This is the 1st commit message:
    
    Remove emergency overlay
    
    This is the commit message #2:
    
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/945ec499041db73043f745fad3b2a3a01e826081' (2022-01-26)
      → 'github:NixOS/nixpkgs/554d2d8aa25b6e583575459c297ec23750adb6cb' (2022-02-02)

--- flake.nix
+++ flake.nix
@@ -1,18 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-fix.url = "github:midchildan/nixpkgs/fix/noto-cjk-src";
     nix-lib.url = "github:schuelermine/nix-lib/b0";
     nix-symlink.url = "github:schuelermine/nix-symlink/b0";
   };
-  outputs = { self, nixpkgs, nix-symlink, nixpkgs-fix, nix-lib }:
+  outputs = { self, nixpkgs, nix-symlink, nix-lib }:
     with nix-lib; {
       nixosConfigurations.buggeryyacht-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = {
-          nixpkgs-fix = nixpkgs-fix.legacyPackages.x86_64-linux;
-          symlink = nix-symlink.symlink;
-        };
+        specialArgs = { inherit (nix-symlink) symlink; };
         modules = attrs.mapToValues (n: _: ./modules + "/${n}")
           (attrs.filter (n: t: t == "regular") (builtins.readDir ./modules));
       };
--- modules/emergency-overlays.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, nixpkgs-fix, ... }: {
-  nixpkgs.overlays = [
-    # This is to preemptively apply nixpkgs PR #156342 to fix Noto CJK downloads
-    (self: super: {
-      noto-fonts-cjk-sans = nixpkgs-fix.noto-fonts-cjk-sans;
-      noto-fonts-cjk-serif = nixpkgs-fix.noto-fonts-cjk-serif;
-    })
-  ];
-}
