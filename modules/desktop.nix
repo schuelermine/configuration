aeacc74ffcb133658cd0eef0b55a54ee405ff8b3
Anselm Schüler <mail@anselmschueler.com>
2022-03-02 15:48:28 +0100

    Add Yaru-Theme

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -29,6 +29,7 @@
   ];
   environment = {
     systemPackages = with pkgs; [
+      yaru-theme
       stockfish  # Provides chess engine for gnome-chess
       gnome.gnome-tweaks
       qalculate-gtk
