e8e293daf44f361e5c5562e85d8f382342a452b9
Anselm Schüler <mail@anselmschueler.com>
2022-03-28 13:13:16 +0200

    Add a fuckton of fonts

--- config/desktop.nix
+++ config/desktop.nix
@@ -22,16 +22,65 @@
     printing.enable = true;
   };
   fonts.fonts = with pkgs; [
+    agave
+    atkinson-hyperlegible
+    cantarell-fonts
+    cascadia-code
+    cm_unicode
+    fira
+    fira
+    fira-code
+    fira-monoo
+    fixedsys-excelsior
+    gentium
+    go-font
+    hack-font
+    ibm-plex
+    jetbrains-mono
+    julia-mono
+    last-resort
+    lato
+    league-of-moveable-type
+    liberation_ttf
+    liberation-sans-narrow
+    libertine
+    merriweather
+    merriweather-sans
+    montserrat
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
-    jetbrains-mono
-    cascadia-code
-    fira-code
-    hack-font
-    fira
-    libertine
-    ibm-plex
+    open-dyslexic
+    open-fonts
+    open-sans
+    open-sans
+    roboto
+    roboto-mono
+    roboto-slab
+    rubik
+    siji
+    source-code-pro
+    source-han-code-jp
+    source-han-mono
+    source-han-sans
+    source-han-sans-japanese
+    source-han-sans-korean
+    source-han-sans-simplified-chinese
+    source-han-sans-traditional-chinese
+    source-han-serif
+    source-han-serif-japanese
+    source-han-serif-korean
+    source-han-serif-simplified-chinese
+    source-han-serif-traditional-chinese
+    source-sans
+    source-sans-pro
+    source-serif
+    source-serif-pro
+    sudo-font
+    ubm_font_family
+    unifont
+    victor-mono
+    vollkorn
   ];
   environment = {
     systemPackages = with pkgs; [
