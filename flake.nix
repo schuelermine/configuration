53ed9173b41216456d8862d34b639a13e2f175fb
Anselm Schüler <mail@anselmschueler.com>
2022-06-07 22:17:00 +0200

    Update usage of nix-lbi

--- flake.nix
+++ flake.nix
@@ -3,13 +3,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-lib.url = "github:schuelermine/nix-lib/b0";
   };
-  outputs = { self, nixpkgs, nix-lib }:
-    with nix-lib; {
+  outputs = { self, nixpkgs, nix-lib }: {
       nixosConfigurations.buggeryyacht = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit nixpkgs; };
-        modules = attrs.mapToValues (n: _: ./config + "/${n}")
-          (attrs.filter (n: t: t == "regular") (builtins.readDir ./config));
+        modules = nix-lib.mapAttrsToValues (n: _: ./config + "/${n}")
+          (nix-lib.filterAttrs (n: t: t == "regular") (builtins.readDir ./config));
       };
     };
 }
