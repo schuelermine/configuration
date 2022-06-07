639208cc96832ebbc8c62e99848becf743d7d407
Anselm Schüler <mail@anselmschueler.com>
2022-06-07 22:18:16 +0200

    Fix

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
       nixosConfigurations.buggeryyacht = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit nixpkgs; };
-        modules = nix-lib.mapAttrsToValues (n: _: ./config + "/${n}")
-          (nix-lib.filterAttrs (n: t: t == "regular") (builtins.readDir ./config));
+        modules = with nix-lib.lib; mapAttrsToValues (n: _: ./config + "/${n}")
+          (filterAttrs (n: t: t == "regular") (builtins.readDir ./config));
       };
     };
 }
