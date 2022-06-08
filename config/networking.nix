1ec8d5da75bd13b010f5bbe3292c268a74b19284
Anselm Schüler <mail@anselmschueler.com>
2022-06-08 12:56:16 +0200

    Add systemd-resolved

---
+++ config/networking.nix
@@ -0,0 +1,7 @@
+{ pkgs, lib, config, ... }: {
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+  };
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+}
