b32ed99fe8e4744c78b4270bfa8ca7e8949064eb
Anselm Schüler <mail@anselmschueler.com>
2022-06-08 13:25:13 +0200

    Try to fix systemd-resolved

--- config/networking.nix
+++ config/networking.nix
@@ -3,5 +3,10 @@
     enable = true;
     dnssec = "true";
   };
-  networking.nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+  networking.nameservers = [
+    "1.1.1.1#cloudflare-dns.com"
+    "1.0.0.1#cloudflare-dns.com"
+    "2606:4700:4700::1111#cloudflare-dns.com"
+    "2606:4700:4700::1001#cloudflare-dns.com"
+  ];
 }
