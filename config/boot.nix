b66d2d61fa4c5c36916da2118dafc5fff50ac056
Anselm Schüler <mail@anselmschueler.com>
2022-08-04 17:06:50 +0200

    Enable Plymouth

--- config/boot.nix
+++ config/boot.nix
@@ -9,6 +9,10 @@
       efi.canTouchEfiVariables = true;
     };
     kernelPackages = pkgs.linuxPackages_latest;
+    plymouth = {
+      enable = true;
+      font = "${pkgs.fira}/share/fonts/opentype/FiraSans-Regular.otf";
+    };
   };
   systemd.targets.network-online.wantedBy = lib.mkForce [ ];
 }
