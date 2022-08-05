9bdddb00bf46ec6c3653fcdd3347c800c91203c9
Anselm Schüler <mail@anselmschueler.com>
2022-08-05 20:56:49 +0200

    Fix that

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-lib.url = "github:schuelermine/nix-lib/b0";
-    base-modules = "github:schuelermine/nixos-base-modules";
+    base-modules.url = "github:schuelermine/nixos-base-modules";
   };
   outputs = { self, nixpkgs, nix-lib, base-modules }: {
     nixosConfigurations.buggeryyacht = nixpkgs.lib.nixosSystem {
