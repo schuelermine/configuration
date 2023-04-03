5db4e67208ad3942fd0f33c1a74a045bfc2a21e9
Anselm Schüler <mail@anselmschueler.com>
2023-04-03 23:39:38 +0200

    Fix locale‽

--- nixosModules/configuration.nix
+++ nixosModules/configuration.nix
@@ -28,10 +28,16 @@
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
       LC_TIME = "en_GB.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_TELEPHONE = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_COLLATE = "en_US.UTF-8";
       LC_MONETARY = "de_DE.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
     };
   };
   console = {
