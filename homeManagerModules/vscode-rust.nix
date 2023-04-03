c758eb95215105d00714d1619db765312844925e
Anselm Schüler <mail@anselmschueler.com>
2023-04-03 20:39:23 +0200

    Revert "anselmschueler: vscode-rust: [rust]."editor.formatOnType" = true"
    This reverts commit ab6f660d284e73564bad20c9ab8248b65074d551.

--- homeManagerModules/vscode-rust.nix
+++ homeManagerModules/vscode-rust.nix
@@ -3,7 +3,7 @@
     vscode = {
       extensions = with pkgs.vscode-extensions; [ rust-lang.rust-analyzer ];
       userSettings = {
-        "[rust]"."editor.formatOnType" = true;
+        "[rust]"."editor.formatOnSave" = true;
         "rust-analyzer.server.path" =
           "${config.programs.rust.rust-analyzer.package}/bin/rust-analyzer";
       };
