486cacbcb31dcb2d4d19ec17568d6f55ab2bd71e
Anselm Schüler <mail@anselmschueler.com>
2023-04-30 15:43:05 +0200

    anselmschueler: switch vadimcn.vscode-lldb to come from pkgs

--- homeManagerModules/vscode-cpp.nix
+++ homeManagerModules/vscode-cpp.nix
@@ -1,12 +1,13 @@
 { pkgs, input-nix-vscode-extensions, ... }: {
   programs.vscode = {
-    extensions = with input-nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
+    extensions = (with input-nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
       llvm-vs-code-extensions.vscode-clangd
       ms-vscode.cmake-tools
       ms-vscode.makefile-tools
       twxs.cmake
+    ]) ++ (with pkgs.vscode-extensions; [
       vadimcn.vscode-lldb
-    ];
+    ]);
     userSettings = {
       "clangd.path" = "${pkgs.clang-tools}/bin/clangd";
       "cmake.cmakePath" = "${pkgs.cmake}/bin/cmake";
