a03a03beae01446d05d0f39d92b753b28084f355
Anselm Schüler <mail@anselmschueler.com>
2023-08-09 14:57:06 +0200

    anselmschueler: Disable HM news

--- homeManagerModules/home-anselmschueler.nix
+++ homeManagerModules/home-anselmschueler.nix
@@ -1,6 +1,7 @@
 {
   nixpkgs.config.allowUnfreePredicate = (_: true);
   programs.home-manager.enable = true;
+  news.display = "silent";
   home = {
     homeDirectory = "/home/anselmschueler";
     username = "anselmschueler";
