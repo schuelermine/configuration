8f512fffd9fb7f8dd9ab93d226f5649d0dbc58e4
Anselm Schüler <mail@anselmschueler.com>
2024-05-29 17:24:32 +0200

    Revert "nailbox: try disabling the framework kmod"
    This reverts commit 0da22d6f500aad19570835896d77c9ee9051d4bf.

--- nixosModules/hardware/nailbox.nix
+++ nixosModules/hardware/nailbox.nix
@@ -26,10 +26,7 @@
     fwupd.enable = true;
     xserver.synaptics.palmDetect = true;
   };
-  hardware = {
-    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-    framework.enableKmod = false;
-  };
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   disko.devices.disk.nvme0n1 = {
     device = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_2000GB_23514Y806477";
     type = "disk";
