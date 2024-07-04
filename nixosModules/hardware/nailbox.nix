ad5e6ac969faed15732b9834278486d29bc28e3c
Anselm Schüler <mail@anselmschueler.com>
2024-07-04 17:51:25 +0200

    Revert "Reapply "remove libinput quirk""
    This reverts commit 0c9312375d034c662552def1f0c9663fad7b4e7f.

--- nixosModules/hardware/nailbox.nix
+++ nixosModules/hardware/nailbox.nix
@@ -60,4 +60,10 @@
       };
     };
   };
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [Logitech MX Master 3S]
+    MatchVendor=0x046D
+    MatchProduct=0xC548
+    AttrEventCode=-REL_WHEEL_HI_RES;-REL_HWHEEL_HI_RES;
+  '';
 }
