a7f16e1067bac44aa7e636b894f07d7ef093f939
Anselm Schüler <mail@anselmschueler.com>
2024-08-08 23:39:02 +0200

    communism

--- source/prompt.fish
+++ source/prompt.fish
@@ -6,12 +6,12 @@ or set -g fish_color_status --background=red white
 
 # Color the prompt differently when we're root
 set -l color_cwd $fish_color_cwd
-set -l suffix '$'
+set -l suffix '☭'
 if functions -q fish_is_root_user; and fish_is_root_user
     if set -q fish_color_cwd_root
         set color_cwd $fish_color_cwd_root
     end
-    set suffix '#'
+    set suffix '★'
 end
 
 # Write pipestatus
@@ -26,4 +26,4 @@ set -l status_color (set_color $fish_color_status)
 set -l statusb_color (set_color $bold_flag $fish_color_status)
 set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+echo -n -s -- (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
