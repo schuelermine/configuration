b8595a2d756a80e9900b2e0678ac37cce873ba0a
Anselm Schüler <mail@anselmschueler.com>
2025-01-06 13:29:46 +0100

    change DNS server

--- nixosModules/configuration.nix
+++ nixosModules/configuration.nix
@@ -38,10 +38,10 @@ in
     dhcpcd.denyInterfaces = lib.mkIf (!machine-vm) [ incus-interface ];
     nftables.enable = true;
     nameservers = [
-      "1.1.1.1#cloudflare-dns.com"
-      "1.0.0.1#cloudflare-dns.com"
-      "2606:4700:4700::1111#cloudflare-dns.com"
-      "2606:4700:4700::1001#cloudflare-dns.com"
+      "9.9.9.9#dns.quad9.net"
+      "149.112.112.112#dns.quad9.net"
+      "2620:fe::fe#dns.quad9.net"
+      "2620:fe::9#dns.quad9.net"
     ];
     networkmanager.enable = true;
     firewall.interfaces.${incus-interface} = lib.mkIf (!machine-vm) {
