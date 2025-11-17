5d5aa4c36d4497182eaa37c43d2ba90bb9db34c2
Anselm Schüler <mail@anselmschueler.com>
2025-11-17 20:28:20 +0100

    fix syntax error

--- home/configuration.nix
+++ home/configuration.nix
@@ -144,7 +144,7 @@ import ../mkMerge${"'"}.nix lib [
         blender-hip
         qbittorrent
         ausweisapp
-      ])
+      ]);
     fonts.fontconfig.enable = true;
     xdg.configFile = {
       "discord/settings.json".text = ''
