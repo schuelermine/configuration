7fc44c6fd06ac19f608df52fbdde913552f353de
Anselm Schüler <mail@anselmschueler.com>
2025-11-23 17:02:48 +0100

    move almost all extensions to a file mostly generated by the script in nixpkgs

--- home/configuration.nix
+++ home/configuration.nix
@@ -13,11 +13,6 @@ import ../mkMerge${"'"}.nix lib [
     nix.package = pkgs.nix;
   }
   {
-    programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions; [
-      ms-vscode-remote.remote-containers
-    ];
-  }
-  {
     programs = {
       haskell = {
         ghc = {
@@ -278,10 +273,6 @@ import ../mkMerge${"'"}.nix lib [
   {
     programs.vscode = {
       profiles.default.extensions = with pkgs.vscode-extensions; [
-        llvm-vs-code-extensions.vscode-clangd
-        ms-vscode.cmake-tools
-        ms-vscode.makefile-tools
-        twxs.cmake
         vadimcn.vscode-lldb
       ];
       profiles.default.userSettings = {
@@ -294,10 +285,6 @@ import ../mkMerge${"'"}.nix lib [
   {
     programs = {
       vscode = {
-        profiles.default.extensions = with pkgs.vscode-extensions; [
-          haskell.haskell
-          justusadam.language-haskell
-        ];
         profiles.default.userSettings = {
           "haskell.serverExecutablePath" =
             "${config.programs.haskell.hls.package}/bin/haskell-language-server-wrapper";
@@ -308,13 +295,6 @@ import ../mkMerge${"'"}.nix lib [
   }
   {
     programs.vscode = {
-      profiles.default.extensions = with pkgs.vscode-extensions; [
-        redhat.java
-        vscjava.vscode-java-debug
-        vscjava.vscode-java-dependency
-        vscjava.vscode-java-test
-        vscjava.vscode-maven
-      ];
       profiles.default.userSettings = {
         "java.configuration.runtimes" = [
           {
@@ -335,13 +315,6 @@ import ../mkMerge${"'"}.nix lib [
   }
   {
     programs.vscode = {
-      profiles.default.extensions = with pkgs.vscode-extensions; [
-        redhat.java
-        vscjava.vscode-java-debug
-        vscjava.vscode-java-dependency
-        vscjava.vscode-java-test
-        vscjava.vscode-maven
-      ];
       profiles.default.userSettings = {
         "java.configuration.runtimes" = [
           {
@@ -362,7 +335,6 @@ import ../mkMerge${"'"}.nix lib [
   }
   {
     programs.vscode = {
-      profiles.default.extensions = with pkgs.vscode-extensions; [ jnoortheen.nix-ide ];
       profiles.default.userSettings = {
         "[nix]"."editor.tabSize" = 2;
         "nix.enableLanguageServer" = true;
@@ -373,23 +345,6 @@ import ../mkMerge${"'"}.nix lib [
   }
   {
     programs.vscode = {
-      profiles.default.extensions = with pkgs.vscode-extensions; [
-        matangover.mypy
-        ms-python.python
-        ms-pyright.pyright
-        ms-toolsai.jupyter
-        ms-toolsai.jupyter-renderers
-        ms-toolsai.vscode-jupyter-cell-tags
-        ms-toolsai.vscode-jupyter-slideshow
-        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "black-py";
-            publisher = "mikoz";
-            version = "1.0.3";
-            sha256 = "sha256-88Il9kfSahmexBYUCMfA0mlLCel+9JSwkssBcuEFrt4=";
-          };
-        })
-      ];
       profiles.default.userSettings = {
         "mypy.dmypyExecutable" = "${config.programs.python.mypy.package}/bin/dmypy";
         "python.defaultInterpreterPath" = "${config.programs.python.package}/bin/python";
@@ -401,7 +356,6 @@ import ../mkMerge${"'"}.nix lib [
   {
     programs = {
       vscode = {
-        profiles.default.extensions = with pkgs.vscode-extensions; [ rust-lang.rust-analyzer ];
         profiles.default.userSettings = {
           "[rust]"."editor.formatOnSave" = true;
           "rust-analyzer.server.path" = "${config.programs.rust.rust-analyzer.package}/bin/rust-analyzer";
@@ -413,62 +367,10 @@ import ../mkMerge${"'"}.nix lib [
   {
     programs.vscode = {
       enable = true;
-      mutableExtensionsDir = true;
+      mutableExtensionsDir = false;
       profiles.default.extensions = with pkgs.vscode-extensions; [
-        bmalehorn.vscode-fish
-        editorconfig.editorconfig
-        firefox-devtools.vscode-firefox-debug
-        github.vscode-pull-request-github
-        mkhl.direnv
-        ms-vscode.hexeditor
-        redhat.vscode-xml
-        redhat.vscode-yaml
-        # streetsidesoftware.code-spell-checker
-        thenuprojectcontributors.vscode-nushell-lang
-        tamasfe.even-better-toml
         myriad-dreamin.tinymist
-        maximedenes.vscoq
-        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "vscode-deno";
-            publisher = "denoland";
-            version = "3.45.2";
-            sha256 = "sha256-U83RWIIorJdFuhr0/l2bIo5JthTFIvedWq52dsSGOx8=";
-          };
-        })
-        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "agda-mode";
-            publisher = "banacorn";
-            version = "0.4.7";
-            sha256 = "sha256-gNa3n16lP3ooBRvGaugTua4IXcIzpMk7jBYMJDQsY00=";
-          };
-        })
-        /* (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "code-spell-checker-german";
-            publisher = "streetsidesoftware";
-            version = "2.3.1";
-            sha256 = "sha256-LxgftSpGk7+SIUdZcNpL7UZoAx8IMIcwPYIGqSfVuDc=";
-          };
-        }) */
-        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "swift-vscode";
-            publisher = "swiftlang";
-            version = "2.14.0";
-            sha256 = "sha256-PvD+5z0FQzw0vdX2Jr42ZvBogNpm0nafuCulOav6P8o=";
-          };
-        })
-        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "language-koka";
-            publisher = "koka";
-            version = "3.1.2";
-            sha256 = "sha256-S4nB80zGO0mYI6y+K3fyB/BHsYU6yZCH4baY7A1M4z8=";
-          };
-        })
-        (pkgs.vscode-utils.buildVscodeExtension rec {
+        /* (pkgs.vscode-utils.buildVscodeExtension rec {
           pname = vscodeExtUniqueId;
           vscodeExtPublisher = "jeanp413";
           vscodeExtName = "open-remote-ssh";
@@ -481,36 +383,7 @@ import ../mkMerge${"'"}.nix lib [
             hash = "sha256-QfJnAAx+kO2iJ1EzWoO5HLogJKg3RiC3hg1/u2Jm6t4=";
             name = "${vscodeExtPublisher}-${vscodeExtName}.zip";
           };
-        })
-        (pkgs.vscode-utils.buildVscodeExtension rec {
-          pname = vscodeExtUniqueId;
-          vscodeExtPublisher = "slevesque";
-          vscodeExtName = "shader";
-          vscodeExtUniqueId = "${vscodeExtPublisher}.${vscodeExtName}";
-          version = "1.1.5";
-          name = "${vscodeExtPublisher}-${vscodeExtName}-${version}";
-          src = pkgs.fetchurl {
-            url = "https://open-vsx.org/api/${vscodeExtPublisher}/${vscodeExtName}/${version}/file/${vscodeExtUniqueId}-${version}.vsix";
-            hash = "sha256-QzqQ/5H08nJPLGwXZKWdCUUqblAe0umYSQbUtEOZNyg=";
-            name = "${vscodeExtPublisher}-${vscodeExtName}.zip";
-          };
-        })
-        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "vscode-containers";
-            publisher = "ms-azuretools";
-            version = "2.0.3";
-            sha256 = "sha256-MAeE99XmjIjYbr72UymnkrDKsNRSjNiB1jdffKTosHQ=";
-          };
-        })
-        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "gitlab-workflow";
-            publisher = "GitLab";
-            version = "6.44.2";
-            sha256 = "sha256-Op62F/EwhRAx+IZTKWjsa0HKPjtf1kPm3VFlFBAHC3I=";
-          };
-        })
+        }) */
       ];
       package = pkgs.vscodium;
       profiles.default.userSettings = {
@@ -564,5 +437,18 @@ import ../mkMerge${"'"}.nix lib [
         "[typst]"."editor.wordSeparators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
       };
     };
+  }
+  {
+    programs.vscode.profiles.default.extensions =
+      let ignored = [{
+        name = "tinymist";
+        publisher = "myriad-dreamin";
+      } {
+        name = "vscode-lldb";
+        publisher = "vadimcn";
+      }]; in
+       lib.map (ext: pkgs.vscode-utils.buildVscodeMarketplaceExtension { mktplcRef = ext; })
+       (lib.filter ({ name, publisher, ... }: !lib.elem { inherit name publisher; } ignored)
+        (import ./extensions.nix).extensions);
   }
 ]
