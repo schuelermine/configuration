e47adb84f09969f9b4fdc18de0c860edc9ec734d
Anselm Schüler <mail@anselmschueler.com>
2025-12-17 19:53:39 +0100

    remove easyeffects temporarily

--- home/nailbox.nix
+++ home/nailbox.nix
@@ -1,4 +1,4 @@
 {
-  services.easyeffects.enable = true;
-  xdg.configFile."easyeffects/output/cab-fw.json".source = ../supplementary/cab-fw.json;
+  /* services.easyeffects.enable = true;
+  xdg.configFile."easyeffects/output/cab-fw.json".source = ../supplementary/cab-fw.json; */
 }
