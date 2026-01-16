915a3a034dd966d6ee777cad3a6237c183c8ea50
Anselm Schüler <mail@anselmschueler.com>
2026-01-16 23:48:42 +0100

    Revert "Reapply "Reapply "remove libinput quirk"""
    This reverts commit 0c23dd00acf308389ff72cb7f666d3e53422f4e1.

--- system/nailbox.nix
+++ system/nailbox.nix
@@ -52,6 +52,7 @@
       };
     };
   };
+
   swapDevices = [
     {
       size = 10240;
@@ -78,4 +79,12 @@
 
   # switcherooctl
   services.switcherooControl.enable = true;
+
+  # mouse quirks
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [Logitech MX Master 3S]
+    MatchVendor=0x046D
+    MatchProduct=0xC548
+    AttrEventCode=-REL_WHEEL_HI_RES;-REL_HWHEEL_HI_RES;
+  '';
 }
