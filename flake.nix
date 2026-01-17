badf3a3ca3f149aea91c8a2c53abb5eb7b165520
Anselm Schüler <mail@anselmschueler.com>
2026-01-17 03:09:15 +0100

    pin just-the-browser to specific commit (manual update)

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     infuse-nix.url = "git+https://codeberg.org/amjoseph/infuse.nix.git";
     infuse-nix.flake = false;
 
-    just-the-browser.url = "github:corbindavenport/just-the-browser";
+    just-the-browser.url = "github:corbindavenport/just-the-browser/8ae206e0a4145e77d488c8e2b740db795ebdf7af";
     just-the-browser.flake = false;
 
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
