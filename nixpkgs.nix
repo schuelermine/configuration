48d64621752f9d0e78b81fe995fcfbfb96b03f86
Anselm Schüler <mail@anselmschueler.com>
2026-01-21 16:40:24 +0100

    temporarily downgrade wireplumber

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -12,6 +12,18 @@ in
           switcheroo-control.__output.nativeBuildInputs.__append = [ prev.wrapGAppsNoGuiHook ];
         }
       )
+      (
+        final: prev: let version = "0.5.12"; in
+        infuse prev {
+          wireplumber.__output = {
+            version.__assign = version;
+            src.__input = {
+              rev.__assign = version;
+              hash.__assign = "sha256-3LdERBiPXal+OF7tgguJcVXrqycBSmD3psFzn4z5krY=";
+            };
+          };
+        }
+      )
     ];
   };
 }
