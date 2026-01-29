620ece9b9a89f1a54e6a8ec091638c18c872143b
Anselm Schüler <mail@anselmschueler.com>
2026-01-29 17:47:36 +0100

    remove wireplumber downgrade

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -12,20 +12,6 @@ in
           switcheroo-control.__output.nativeBuildInputs.__append = [ prev.wrapGAppsNoGuiHook ];
         }
       )
-      (
-        # https://github.com/NixOS/nixpkgs/issues/475202
-        # TODO: remove
-        final: prev: let version = "0.5.12"; in
-        infuse prev {
-          wireplumber.__output = {
-            version.__assign = version;
-            src.__input = {
-              rev.__assign = version;
-              hash.__assign = "sha256-3LdERBiPXal+OF7tgguJcVXrqycBSmD3psFzn4z5krY=";
-            };
-          };
-        }
-      )
     ];
   };
 }
