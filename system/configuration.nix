823781edfb4d67d1e3778a8d792640a58ee11c5a
Anselm Schüler <mail@anselmschueler.com>
2026-02-07 20:56:39 +0100

    remove minecraft port share

--- system/configuration.nix
+++ system/configuration.nix
@@ -108,10 +108,10 @@ import ../mkMerge${"'"}.nix lib [
           to = 42001;
         };
         ausweisapp = 24727;
-        minecraft = 44923;
       in
       {
         enable = true;
+        # logRefusedPackets = true;
         interfaces.${incus-interface} = {
           allowedTCPPortRanges = [
             {
@@ -136,8 +136,8 @@ import ../mkMerge${"'"}.nix lib [
           kdeconnect
           warpinator
         ];
-        allowedTCPPorts = [ ausweisapp minecraft ];
-        allowedUDPPorts = [ ausweisapp minecraft ];
+        allowedTCPPorts = [ ausweisapp ];
+        allowedUDPPorts = [ ausweisapp ];
       };
   }
   {
