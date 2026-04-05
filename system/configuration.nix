86424ee4f0733503ab3891c110762bbba6bab735
Anselm Schüler <mail@anselmschueler.com>
2026-04-05 03:16:36 +0200

    switch to realtime kernel

--- system/configuration.nix
+++ system/configuration.nix
@@ -38,7 +38,7 @@ import ../mkMerge${"'"}.nix lib [
     boot = {
       kernel.sysctl."kernel.sysrq" = 1;
       binfmt.emulatedSystems = [ "aarch64-linux" ];
-      kernelPackages = pkgs.linuxPackages_latest;
+      kernelPackages = pkgs.linuxPackages-rt_latest;
       supportedFilesystems = [
         "ntfs"
         "exfat"
