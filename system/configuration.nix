02e592ea1d1b4db7191a35af4061e5f599dd79d8
Anselm Schüler <mail@anselmschueler.com>
2026-06-03 11:48:36 +0200

    disable libvirt for the moment

--- system/configuration.nix
+++ system/configuration.nix
@@ -481,7 +481,7 @@ import ../mkMerge${"'"}.nix lib [
     # other virtualisation
     virtualisation = {
       libvirtd = {
-        enable = true;
+        enable = false;
         qemu.swtpm.enable = true;
       };
       podman = {
