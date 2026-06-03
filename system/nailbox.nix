7226a955e8243dd330ad2873a2fb7de933d4ec01
Anselm Schüler <mail@anselmschueler.com>
2026-06-03 13:02:10 +0200

    disable kernel patch & use upstream nixos-hardware

--- system/nailbox.nix
+++ system/nailbox.nix
@@ -91,7 +91,7 @@
   '';
 
   # kernel patch to work around https://gitlab.freedesktop.org/drm/amd/-/work_items/5163
-  boot.kernelPatches = [
+  boot.kernelPatches = lib.mkIf false [
     {
       name = "drm/amd/display: Disable panel replay feature unconditionally to work around https://gitlab.freedesktop.org/drm/amd/-/work_items/5163";
       patch = ../supplementary/0001-drm-amd-display-Disable-panel-replay-feature-uncondi.patch;
