c58ff2982c7c7d3a7e8bea9421295944900c0d29
Anselm Schüler <mail@anselmschueler.com>
2026-06-03 14:23:28 +0200

    Revert "remove switcherooctl"
    This reverts commit bb74dd2c6d67e90b6efb4d09cf4935d63ac96285.

--- system/nailbox.nix
+++ system/nailbox.nix
@@ -77,6 +77,9 @@
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   services.fwupd.enable = true;
 
+  # switcherooctl
+  services.switcherooControl.enable = true;
+
   # mouse quirks
   environment.etc."libinput/local-overrides.quirks".text = ''
     [Logitech MX Master 3S]
