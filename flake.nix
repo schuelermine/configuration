3749be031aa7ea5694eeb3b46387abd488a9519f
Anselm Schüler <mail@anselmschueler.com>
2026-06-13 15:31:29 +0200

    go back to upstream nixos-hardware again

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
-    nixos-hardware.url = "github:schuelermine/nixos-hardware/framework-16-remove-quirks";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
     home-manager.url = "github:nix-community/home-manager";
     xhmm.url = "github:schuelermine/xhmm/b0";
     fenix.url = "github:nix-community/fenix";
