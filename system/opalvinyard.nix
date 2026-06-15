018b0b13307e50742b3037e63c48672d671a3e27
Anselm Schüler <mail@anselmschueler.com>
2026-06-15 23:55:13 +0200

    try disabling gpu mitigations

--- system/opalvinyard.nix
+++ system/opalvinyard.nix
@@ -91,8 +91,6 @@
     AttrEventCode=-REL_WHEEL_HI_RES;-REL_HWHEEL_HI_RES;
   '';
 
-  boot.kernelParams = [ "amdgpu.dcdebugmask=0x410" ];
-
   # kernel patch to work around https://gitlab.freedesktop.org/drm/amd/-/work_items/5163
   boot.kernelPatches = lib.mkIf false [
     {
