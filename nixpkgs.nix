cf0e1575f14309ad8011419f5bd3bf8101e1e2cf
Anselm Schüler <mail@anselmschueler.com>
2026-06-26 16:30:28 +0200

    use stable nixpkgs for cine

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,10 +1,15 @@
 { inputs, lib, ... }:
 let
   inherit ((import inputs.infuse-nix { inherit lib; }).v1) infuse;
+  
 in
 {
   nixpkgs = {
     config.allowUnfree = true;
-    overlays = [];
+    overlays = [
+      (final: prev: {
+        cine = (import inputs.nixpkgs-stable { inherit (prev.stdenv.hostPlatform) system; }).cine;
+      })
+    ];
   };
 }
